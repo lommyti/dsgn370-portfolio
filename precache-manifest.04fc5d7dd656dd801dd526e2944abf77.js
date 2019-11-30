0fc72a060783c93fc43b8b019f2c8c004a2f7fd0
lommyti <thomasli2020@u.northwestern.edu>
2019-11-30 04:12:04 -0500

    Updates

---
+++ precache-manifest.04fc5d7dd656dd801dd526e2944abf77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0478a0db9be0a7fd0192a0db48d94cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d25338a53dabca54338",
+    "url": "/static/css/main.7f8ef3c2.chunk.css"
+  },
+  {
+    "revision": "075c9bc88a5af6648479",
+    "url": "/static/js/2.12a2843e.chunk.js"
+  },
+  {
+    "revision": "1d25338a53dabca54338",
+    "url": "/static/js/main.61df0e41.chunk.js"
+  },
+  {
+    "revision": "561b08e45b963bf64fcb",
+    "url": "/static/js/runtime-main.05080bb6.js"
+  }
+]);
